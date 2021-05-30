b5c8f97d9751acc4e047cb469204bba6f581ee31
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-05-30 20:49:53 +0600

    finish with app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,70 @@
-import React from 'react';
+import React, { useState } from "react";
+import keys from "./keys";
+
 const api = {
-  key: "eaa8953efc60bac2b895ab04599da06a",
-  base: "api.openweathermap.org/data/2.5/"
-}
+  key: keys.API_KEY,
+  base: keys.BASE_URL,
+};
 
 function App() {
+  const dateBuild = (d) => {
+    let date = String(new window.Date());
+    date = date.slice(3, 15);
+    return date;
+  };
 
-  const CreateDate = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`
-  }
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+        });
+    }
+  };
 
   return (
-    <div className="app">
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 18
+            ? "App hot"
+            : "App cold"
+          : "App"
+      }
+    >
       <main>
-        <div className="search-box">
+        <div className="search-container">
           <input
             type="text"
+            placeholder="Search..."
             className="search-bar"
-            placeholder="Search..." />
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-        <div className="location-box">
-          <div className="location">Dhaka, Bangladesh</div>
-          <div className="date">{CreateDate(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">15°c</div>
-          <div className="weather">Sunny</div>
-        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-container">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date"> {dateBuild(new Date())}</div>
+            </div>
+            <div className="weather-container">
+              <div className="temperature">
+                {Math.round(weather.main.temp)}°C
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
---
+++ src/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  API_KEY: "34a9443ec78995fb63771e752179c70b",
+  BASE_URL: "https://api.openweathermap.org/data/2.5/",
+};
