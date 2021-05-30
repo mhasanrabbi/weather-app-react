dea329ab278b45b840f7b2e308f10079ab72b8e6
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-05-30 14:39:15 +0600

    create location and weather container

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ const api = {
 }
 
 function App() {
+
+  const CreateDate = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app">
       <main>
@@ -13,6 +27,14 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">Dhaka, Bangladesh</div>
+          <div className="date">{CreateDate(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°c</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
